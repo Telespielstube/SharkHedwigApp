39d9bba5ce85d47f07e9346befe825f2989b3620
Marta Bruening <martabruening@icloud.com>
2024-05-11 23:26:08 +0200

    Renaming some files.

--- src/main/java/net/sharkhedwigapp/Application.java
+++ src/main/java/net/sharkhedwigapp/Application.java
@@ -1,18 +1,17 @@
 package net.sharkhedwigapp;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1440, 900);
-        stage.setTitle("Hello!");
+        stage.setTitle("SharkHedwigApp");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
--- src/main/java/net/sharkhedwigapp/Controller.java
+++ src/main/java/net/sharkhedwigapp/Controller.java
@@ -1,11 +1,9 @@
 package net.sharkhedwigapp;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label main_label;
 
